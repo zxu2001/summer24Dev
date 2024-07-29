8bcb4e75938fa1791a14fe96c06e5bc659554490
zxu2001 <32138202+zxu2001@users.noreply.github.com>
2024-07-29 20:29:12 +0000

    largest.java

---
+++ java/Largest.java
@@ -0,0 +1,16 @@
+public class Largest {
+	public Largest() {
+    }
+    
+    public int largest(int[] list){
+        int index;
+        int max =0;
+        for (index = 0; index < list.length-1; index++){
+            if (list[index] > max) {
+                max = list[index];
+            }
+        }
+        return max;
+    }
+
+}
