655c420fc7c9bd1f42744f05246442d365d57c2f
zwxu@umich.edu <32138202+zxu2001@users.noreply.github.com>
2024-09-30 18:01:07 +0000

    Add more example

--- CIS476/Builder/BuildExample.java
+++ CIS476/Builder/BuildExample.java
@@ -4,8 +4,19 @@ public class BuildExample {
         AerospaceEngineer mydirector = new AerospaceEngineer();
 
         AirplaneBuilder crop=new CropDuster();
+        AirplaneBuilder fighter = new FighterJet();
+        AirplaneBuilder glider = new Glider();
+        
         mydirector.constructAirplane(crop);
         Airplane completedCropDuster = crop.GetAirplane();
         completedCropDuster.show();
+
+        mydirector.constructAirplane(fighter);
+        Airplane completedFighterJet = fighter.GetAirplane();
+        completedFighterJet.show();
+
+        mydirector.constructAirplane(glider);
+        Airplane completedGlider = glider.GetAirplane();
+        completedGlider.show();
     }
 }
