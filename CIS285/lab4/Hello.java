a7101a78360ee73ef20e38d61a37cf2502d451f4
zwxu@umich.edu <32138202+zxu2001@users.noreply.github.com>
2024-10-01 18:17:27 +0000

    coop 10 main

---
+++ CIS285/lab4/Hello.java
@@ -0,0 +1,10 @@
+package CIS285.lab4;
+
+public class Hello {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.println("Hello World " + i);
+        }
+    }
+    
+}
