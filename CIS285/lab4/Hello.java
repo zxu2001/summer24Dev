3d6c1fbc9949853ee231d2f6c20d00914a644b0b
zwxu@umich.edu <32138202+zxu2001@users.noreply.github.com>
2024-10-01 18:46:58 +0000

    loop 13

--- CIS285/lab4/Hello.java
+++ CIS285/lab4/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        for(int i=1;i<=12;i++){
+        for(int i=1;i<=13;i++){
             System.out.println("Hello World " + i);
         }
     }
