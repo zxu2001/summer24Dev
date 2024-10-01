88c4eb316420d4ddf24878360bee927708ef9232
zwxu@umich.edu <32138202+zxu2001@users.noreply.github.com>
2024-10-01 18:18:36 +0000

    tom changed to loop 11

--- CIS285/lab4/Hello.java
+++ CIS285/lab4/Hello.java
@@ -2,7 +2,7 @@ package CIS285.lab4;
 
 public class Hello {
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
+        for(int i=1;i<=11;i++){
             System.out.println("Hello World " + i);
         }
     }
